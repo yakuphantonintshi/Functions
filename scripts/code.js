e8252988a14d097832703706ef0267f4750f46be
yakuphantonintshi <yakupha58756@gmail.com>
2024-05-23 09:26:23 +0200

    Add Functions

---
+++ scripts/code.js
@@ -0,0 +1,25 @@
+// Create a function called "greet" that accepts a single parameter. Log the parameter along with a "Hello".
+
+function greet(name){
+    console.log('Hello')
+}
+greet ()
+
+//Create a function called multiply. It should accept two parameters and log the multiplication of the two given parameters.
+
+function multiply(numb1 , numb2){
+    console.log(numb1 * numb2)
+}
+multiply(7 , 7)
+
+//Create a function called tableOfFive which accepts a single parameter. Return the parameter multiplied by five to a variable, and log this variable.
+
+function tableOfFive(numb){
+    let output = numb * 5
+    console.log(output);
+
+}
+tableOfFive(2)
+tableOfFive(9)
+tableOfFive(8)
+tableOfFive(4)
